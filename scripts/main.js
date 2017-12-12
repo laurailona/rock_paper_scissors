d238745cddea1e0f8181c525ce8e86fe5341a1f9
Laura Ilona <clarkelaura92@gmail.com>
2017-12-12 22:23:56 +0200

    Add basic DOM functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var rpsArray = ["rock", "paper", "scissors"]
 var computerMove = "";
-var result = "";
+var result;
 var myMove = "Rock";
 var computerScore = 0;
 var myScore = 0;
@@ -8,7 +8,44 @@ var scoreBoard = "";
 var winner = "";
 var computerPlay;
 var game;
+var counter = 1;
 
+//DOM-stuff
+var roundNumberElement = document.getElementById("round-number");
+var myScoreElement = document.getElementById("my-score");
+var computerScoreElement = document.getElementById("computer-score");
+var computerMoveElement = document.getElementById("computers-move");
+var myMoveElement = document.getElementById("my-move");
+var resultElement = document.getElementById("result");
+
+//Make-A-Move Buttons
+var buttonRock = document.getElementById("rock");
+var buttonPaper = document.getElementById("paper");
+var buttonScissors = document.getElementById("scissors");
+  
+var myMoveRock = function() {
+  myMove = "rock";
+  game();
+  counter++;
+};
+
+var myMovePaper = function() {
+  myMove = "paper";
+  game();
+  counter++;
+};
+
+var myMoveScissors = function() {
+  myMove = "scissors";
+  game();
+  counter++;
+};
+
+buttonRock.addEventListener("click", myMoveRock);
+buttonPaper.addEventListener("click", myMovePaper);
+buttonScissors.addEventListener("click", myMoveScissors);
+
+//THE GAME
 game = function() { 
   //Create Computer's move
   computerPlay = function() {
@@ -17,16 +54,9 @@ game = function() {
   };
   computerPlay();
 
-  //prompt to say your move
-  myMove = window.prompt("What's it gonna be? Rock, paper, scissors?");
-  myMove = myMove.toLowerCase();
-
   //Determine result
   if (myMove !== "rock" && myMove !== "paper" && myMove !== "scissors") {
     result = "That makes no sense...the game is called 'Rock, Paper, Scissors'!";
-  }
-  else if (computerMove == myMove) {
-    result = "Oh no! It's a draw...try again";
   }
   else if (myMove == "rock" && computerMove == "scissors") {
     result = "You win! Rock beats Scissors!";
@@ -41,40 +71,46 @@ game = function() {
     myScore += 1;
   }
   else if (myMove == "scissors" && computerMove == "rock") {
-    result == "You lose! Rock beats Scissors";
+    result = "You lose! Rock beats Scissors";
     computerScore += 1;
   }
   else if (myMove == "paper" && computerMove == "rock") {
-    result == "You win! Paper beats Rock!";
+    result = "You win! Paper beats Rock!";
     myScore += 1;
   }
-  else if (myMove == "paper" && compuerMove == "scissors") {
-    result == "You lose! Scissors beat paper";
+  else if (myMove == "paper" && computerMove == "scissors") {
+    result = "You lose! Scissors beat paper";
     computerScore += 1;
   }
+  else if (computerMove == myMove) {
+    result = "Oh no! It's a draw...try again";
+  }
+
+  //Display choices
+  myMoveElement.textContent = myMove.toUpperCase();
+  computerMoveElement.textContent = computerMove.toUpperCase();
 
   //Display result
-  console.log(result);
+  roundNumberElement.textContent = counter;
+  resultElement.textContent = result;
 
   //Display current score
-  scoreBoard = `Your score:${myScore} Computer's score: ${computerScore}`;
-  console.log(scoreBoard);
+  myScoreElement.textContent = myScore;
+  computerScoreElement.textContent = computerScore;
+  //Determine and display the winner, reset the score and counter
+  if (counter == 5) {
+    if (myScore > computerScore) {
+      winner = "Congratulations! You won the game!";
+    }
+    else if (myScore == computerScore) {
+      winner = "It's a draw. This game has no winner!";
+    }
+    else if (myScore < computerScore) {
+      winner = "You lost the game!";
+    }
+    alert(winner);
+    counter = 0;
+    myScore = 0;
+    computerScore = 0;
+  }
 };
-
-//Play 5 rounds
-for (let i = 0; i <= 4;i++) {
-  game();
-};
-
-//Determine and display the winner
-if (myScore > computerScore) {
-  winner = "Congratulations! You won the game!";
-}
-else if (myScore == computerScore) {
-  winner = "It's a draw. This game has no winner!";
-}
-else if (myScore < computerScore) {
-  winner = "You lost the game!";
-}
-
-console.log(winner);
