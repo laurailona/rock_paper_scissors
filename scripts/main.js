120a393a3cc4b605a2816188de3fabb10954da0b
Laura Ilona <clarkelaura92@gmail.com>
2017-12-13 11:20:54 +0200

    Add icons, change font and colours

--- scripts/main.js
+++ scripts/main.js
@@ -17,6 +17,8 @@ var computerScoreElement = document.getElementById("computer-score");
 var computerMoveElement = document.getElementById("computers-move");
 var myMoveElement = document.getElementById("my-move");
 var resultElement = document.getElementById("result");
+var myChoiceIcon = document.getElementById("my-choice-icon");
+var computerChoiceIcon = document.getElementById("computer-choice-icon");
 
 //Make-A-Move Buttons
 var buttonRock = document.getElementById("rock");
@@ -93,7 +95,50 @@ game = function() {
   //Display result
   roundNumberElement.textContent = counter;
   resultElement.textContent = result;
+  //Show icons fa-hand-rock-o fa-hand-paper-o fa-hand-rock-o - There is probably a better way of doing this
+  let changeMyIcon = function() {
+    if (myMove == "rock") {
+      myChoiceIcon.classList.remove("fa-spinner");
+      myChoiceIcon.classList.remove("fa-hand-paper-o");
+      myChoiceIcon.classList.remove("fa-hand-scissors-o");
+      myChoiceIcon.classList.add("fa-hand-rock-o");
+    }
+    else if (myMove == "paper") {
+      myChoiceIcon.classList.remove("fa-spinner");
+      myChoiceIcon.classList.remove("fa-hand-rock-o");
+      myChoiceIcon.classList.remove("fa-hand-scissors-o");
+      myChoiceIcon.classList.add("fa-hand-paper-o");
+    }
+    else if (myMove == "scissors") {
+      myChoiceIcon.classList.remove("fa-spinner");
+      myChoiceIcon.classList.remove("fa-hand-paper-o");
+      myChoiceIcon.classList.remove("fa-hand-rock-o");
+      myChoiceIcon.classList.add("fa-hand-scissors-o");
+    }
+  };
+  let changeComputerIcon = function() {
+    if (computerMove == "rock") {
+      computerChoiceIcon.classList.remove("fa-spinner");
+      computerChoiceIcon.classList.remove("fa-hand-paper-o");
+      computerChoiceIcon.classList.remove("fa-hand-scissors-o");
+      computerChoiceIcon.classList.add("fa-hand-rock-o");
+    }
+    else if (computerMove == "paper") {
+      computerChoiceIcon.classList.remove("fa-spinner");
+      computerChoiceIcon.classList.remove("fa-hand-rock-o");
+      computerChoiceIcon.classList.remove("fa-hand-scissors-o");
+      computerChoiceIcon.classList.add("fa-hand-paper-o");
+    }
+    else if (computerMove == "scissors") {
+      computerChoiceIcon.classList.remove("fa-spinner");
+      computerChoiceIcon.classList.remove("fa-hand-paper-o");
+      computerChoiceIcon.classList.remove("fa-hand-rock-o");
+      computerChoiceIcon.classList.add("fa-hand-scissors-o");
+    }
+  };
 
+  changeMyIcon();
+  changeComputerIcon();
   //Display current score
   myScoreElement.textContent = myScore;
   computerScoreElement.textContent = computerScore;
